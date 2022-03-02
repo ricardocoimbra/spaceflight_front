391483e35029dd50978b7f4a205016e60d7f8e75
Ricardo Coimbra <coimbra.rico@gmail.com>
2022-03-02 16:52:28 -0400

    :recycle: Including items for Article
    Incluiding in Article View (Date, url for newsSite and button "view more")

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -2,7 +2,9 @@
       <div class="article d-inline-flex col-10">
         <div class="col-6">
           <h1>{{ article.title }}</h1>
+          <p>{{ moment(article.updatedAt).format('DD/MM/YYYY') }} <a :href="article.url">newsSite</a></p>
           <p class="mt-5">{{article.summary.substring(0,200)+(article.summary.length>200 ? "..." : "")}}</p>
+          <button class="btn btn-info">Ver mais</button>
         </div>
         <div class="image col-6 mb-6">
           <img class="image-responsive" :src="article.imageUrl" :alt="article.title">
@@ -19,7 +21,7 @@ export default {
 
 <style scoped>
 .article {
-  margin: 20px;
+  margin: 40px;
 }
 h1 {
     font-size: 1.5em;
--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import store from './store'
 import bootstrap from './bootstrap'
 import axios from 'axios'
+import moment from 'moment'
 
 Vue.config.productionTip = false
 Vue.use(bootstrap)
+Vue.prototype.moment = moment
+
 Vue.prototype.$http = axios
 
 new Vue({
